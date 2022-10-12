c876c8eb903e70104086f707eaa97b2d81b01eb6
Miguel Leiton <mleitona1@uft.edu>
2022-10-12 16:05:57 -0300

    V0.0.3

---
+++ Listas.py
@@ -0,0 +1,12 @@
+print("Hola desde las listas enlazadas y circulares!")
+
+
+class Lista_S():
+    
+    def __init__(self,aux):
+        self.aux = aux
+        self.link = None
+    def AgregaPrimero(self,agrega):
+        
+        print(" se debe agregar la clase nodo")
+        
